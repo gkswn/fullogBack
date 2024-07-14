0722a60b660bc1cab4cf8edae97543b034026902
gkswn <ahj304819@gmail.com>
2024-07-14 21:33:27 +0900

    init project

---
+++ src/main/java/com/example/fullogback/FullogBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullogBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullogback/FullogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullogBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
