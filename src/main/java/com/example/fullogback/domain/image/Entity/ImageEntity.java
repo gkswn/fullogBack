314a36d54c96cab4f136288c6adac731990db489
gkswn <ahj304819@gmail.com>
2024-07-17 20:28:12 +0900

    수정 - 일대다관계로 post,image 연관관계

--- src/main/java/com/example/fullogback/domain/image/Entity/ImageEntity.java
+++ src/main/java/com/example/fullogback/domain/image/Entity/ImageEntity.java
@@ -1,16 +1,25 @@
 package com.example.fullogback.domain.image.Entity;
 
 import com.example.fullogback.custom.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import com.example.fullogback.domain.post.entity.PostEntity;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 @Getter
 @Builder
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ImageEntity extends BaseEntity {
 
+    @Column
+    private String url;
+
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
 }
 
--- src/main/java/com/example/fullogback/domain/post/entity/PostEntity.java
+++ src/main/java/com/example/fullogback/domain/post/entity/PostEntity.java
@@ -1,8 +1,12 @@
 package com.example.fullogback.domain.post.entity;
 
 import com.example.fullogback.custom.entity.BaseEntity;
-import jakarta.persistence.Entity;
+import com.example.fullogback.domain.image.Entity.ImageEntity;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -10,8 +14,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostEntity extends BaseEntity {
+
+    @Column
     private String title;
+    @Column
     private String content;
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ImageEntity> images = new ArrayList<>();
 
 }
