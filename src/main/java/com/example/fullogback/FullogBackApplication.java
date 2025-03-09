99076463fc4e6437028337fddb3ceca70425aadf
gkswn <ahj304819@gmail.com>
2025-03-09 17:08:29 +0900

    수정 - jpa auditing 버그 수정 FP-8

--- src/main/java/com/example/fullogback/FullogBackApplication.java
+++ src/main/java/com/example/fullogback/FullogBackApplication.java
@@ -2,8 +2,10 @@ package com.example.fullogback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FullogBackApplication {
 
     public static void main(String[] args) {
